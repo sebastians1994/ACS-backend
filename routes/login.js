a4e411a77db9c6580004b1b7df267dc2b700358b
sebastians1994 <40479606+sebastians1994@users.noreply.github.com>
2021-11-17 22:31:20 -0800

    Barebones Node login server

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,89 @@
-var express = require('express'),
-router = express.Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
 
-router.post('/', function(req,res){
-  //Mock login later.
-  res.json({status: "success"});
-});
+const app = express();
+app.use(express.json());
 
-module.exports = router;
+const database = {
+    users:[
+        {
+            id:'123',
+            name:'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+
+        },
+        {
+            id:'123',
+            name:'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+
+app.get('/', (req, res)=>{
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    if(req.body.email === database.users[0].email && 
+        req.body.password === database.users[0].password){
+            res.json('success');
+        } else {
+            res.status(400).json('error logging in');
+
+        }
+  })
+
+
+  app.post('/register', (req, res) => {
+    const { email, name, password, } = req.body;
+    // bcrypt.hash(password, null, null, function(err, hash) {
+    //     console.log(hash);
+    // });
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
+  })
+  
+  app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    const found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            return res.json(user)
+        }
+      })
+      if(!found){
+          res.status(400).json('not found');
+      }
+  })
+  
+  
+app.listen(3000, ()=>{
+    console.log('app is running on port 3000')
+})
+
+
+// var express = require('express'),
+// router = express.Router();
+
+// router.post('/', function(req,res){
+//   //Mock login later.
+//   res.json({status: "success"});
+// });
+
+// module.exports = router;
